37ff4d44b3f12be9c5a30216b60e6d97a6aeaa01
Kevin Rivas <alexanderloaiza@MacBook-Air-de-Alexander.local>
2025-08-15 14:41:50 -0500

    Se sube el archivo VariablesYTipos y el Myself modificados

---
+++ src/main/kotlin/edu/unicatolica/kotlin/Myself.kt
@@ -0,0 +1,13 @@
+package edu.unicatolica.kotlin
+
+fun main (){
+    val name ="Kevin Alexis Rivas Bolaños"
+    val number: Int = 26
+    var program: String ="Tecnolgia en desarrollo de software"
+    val interest = "Soy un estudiante interesado en los sistemas desde que estaba en bachillerato pero ahora me doy cuenta que me gusta mas el desarrollo web por eso me enfoque en el desarollo de software"
+
+
+    val message ="Hola, soy $name tengo $number años estoy inscrito en el programa de $program y $interest "
+
+    println(message)
+}
--- src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
+++ src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
@@ -1,6 +1,34 @@
 // Variables&Types.kt
 package edu.unicatolica.kotlin
 
+
+
 fun main() {
     // variables and data types
+
+    println("Hello Word")
+
+    var name: String = "juan"
+    var name2 = "Juan"
+
+    name = "Juan"
+
+    val number1: Int = 42
+    val number2: Long = 3L
+    val number3: Float = 3.14f
+    val number4: Double = 2.3
+
+
+    val adding = 10 + 1
+    val substracting = 10 - 1
+    val product = 10 * 1
+    val division = 10/1
+    val module = 10 % 3
+
+    val name3 = "Lina"
+    val age = 19
+    val city = "Amaime"
+
+    val message = "Hi, $name3. Your age is $age and you live in $city"
+    println(message)
 }
